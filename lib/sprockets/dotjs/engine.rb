c52398274af6184a10b7c4402ef861a9b4556622
artygus <hero@engineeriam.net>
2017-06-15 12:17:57 +0300

    disabled sprockets deprecation warnings
    set sprocket requirement to "~> 3" because LegacyTiltProcessor is going
    to be removed in 4.x

--- lib/sprockets/dotjs/engine.rb
+++ lib/sprockets/dotjs/engine.rb
@@ -3,7 +3,7 @@ module Sprockets
     class Engine < ::Rails::Engine
       initializer "sprockets.dotjs", :group => :all do |app|
         ::Rails.application.config.assets.configure do |env|
-          env.register_engine(".djs", ::Sprockets::DotJS::Compiler)
+          env.register_engine(".djs", ::Sprockets::DotJS::Compiler, silence_deprecation: true)
         end
       end
     end
